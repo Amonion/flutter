43ed1b64244fdafa8580412f75f347d979720c0d
Amonion <amonionprime@gmail.com>
2024-08-10 08:20:07 +0100

    Updated asset images, added fonts, removed scripts warnings, and add splash screen.

---
+++ lib/utils/constants/image_strings.dart
@@ -0,0 +1,9 @@
+class KImages{
+  //APP LOGOS
+  static const String darkAppLogo = "assets/logos/schoolin-dark-logo.png";
+  static const String lightAppLogo = "assets/logos/schoolin-light-logo.png";
+
+  // SOCIAL LOGOS
+  static const String google = "assets/logos/google-icon.png";
+  static const String facebook = "assets/logos/facebook-icon.png";
+}
---
+++ lib/utils/constants/text_strings.dart
@@ -0,0 +1,14 @@
+class KTexts{
+
+  // ONBOARDING TEXTS
+  static const String onBoardingTitle1 = "Choose your product";
+  static const String onBoardingTitle2 = "Select Payment Method";
+  static const String onBoardingTitle3 = "Deliver at your door step";
+
+  static const String onBoardingSubTitle1 = "Welcome to a world of limitless choices - Your Perfect Product Awaits!";
+  static const String onBoardingSubTitle2 = "For seamless Transactions, Choose your payment path - Your Convenience, Our Priority!";
+  static const String onBoardingSubTitle3 = "From our doorstep to yours - Swift, Secure, and Contactless Delivery";
+
+  static const String homeAppbarTitle = "Good day for shopping";
+  static const String homeAppbarSubTitle = "Mavis Brown";
+}
